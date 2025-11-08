c230f41261a8972747f5412bb146c9735492f98d
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-08 21:30:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Customer Churn Analysis")
 st.write("This project analyzes customer data to understand and predict churn — identifying customers who are likely to discontinue a service. By uncovering key behavioral and demographic factors behind churn, this analysis helps businesses make data-driven retention decisions.")
 tab1,tab2,tab3,tab4,tab5=st.tabs(["Overview","About Data","Visualisation","Findings","Prediction"])
-
+import joblib
 import numpy as np
 import pandas as pd
 df=pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
