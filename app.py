df0493bce02dd9d6fb6f66900060d1a479a43f69
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-08 20:39:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,19 @@ tab1,tab2,tab3,tab4=st.tabs(["Overview","About Data","Visualisation","Prediction
 import numpy as np
 import pandas as pd
 df=pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+num_rows, num_cols=df.shape
+numerical_features=df.select_dtypes(include=np.number).shape[1]
+categorical_features=df.select_dtypes(include='object').shape[1]
+data_structure={
+    'Metric': ['Number of Rows', 'Number of Columns', 'Numerical Features', 'Categorical Features'],
+    'Count': [num_rows, num_cols, numerical_features, categorical_features]
+}
+data_structure_d=pd.DataFrame(data_structure)
 with tab1:
   st.subheader("Dataset Overview")
   st.write(df)
   st.subheader("Basic Statistics")
   st.write(df.describe())
 
+with tab2:
+  st.write(data_structure)
