e57cef0355ff97a21cb8148d65e739a2671b5dd9
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-08 20:26:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 import streamlit as st
 st.title("Customer Churn Analysis")
+st.write("This project analyzes customer data to understand and predict churn — identifying customers who are likely to discontinue a service. By uncovering key behavioral and demographic factors behind churn, this analysis helps businesses make data-driven retention decisions.")
